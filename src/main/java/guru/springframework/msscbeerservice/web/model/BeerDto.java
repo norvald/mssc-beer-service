449264ee948337b3121744212189fdeab7939d2b
Norvald Espedal <norvald.espedal@lyse.net>
2022-11-27 15:06:38 +0100

    Lecture 70

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -13,16 +17,33 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
 
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime lastModifiedDate;
 
+    @NotBlank
     private String beerName;
+
+    @NotNull
     private BeerStyleEnum beerStyle;
+
+    @Positive
+    @NotNull
     private Integer upc;
-    private Integer quantityOnHand;
+
+    @Positive
+    @NotNull
     private String price;
+
+    private Integer quantityOnHand;
 
 }
