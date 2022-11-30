3a8eb4af0d64caeebe254236baaed8ed7174c060
Norvald Espdal <norvald.espedal@lyse.no>
2022-11-30 09:44:35 +0100

    Add beer

--- src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
@@ -15,9 +15,13 @@ public class BeerLoader implements CommandLineRunner {
     public static final String BEER_1_UPC = "0631234200036";
     public static final String BEER_2_UPC = "0631234300019";
     public static final String BEER_3_UPC = "0083783375213";
+    public static final String BEER_4_UPC = "0234892394829";
+    public static final String BEER_5_UPC = "0824289340283";
     public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
     public static final UUID BEER_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
     public static final UUID BEER_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
+    public static final UUID BEER_4_UUID = UUID.fromString("7f287553-0e8f-45a2-8150-0161a95f9842");
+    public static final UUID BEER_5_UUID = UUID.fromString("821e2dcc-7a49-4ee0-bd69-ca91ebbd2ad9");
 
     private final BeerRepository beerRepository;
 
